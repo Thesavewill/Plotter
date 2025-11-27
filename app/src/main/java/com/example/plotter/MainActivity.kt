81cc906657ce5c491841f94722f1c95bffadc21c
Thesavewill <denes3040@gmail.com>
2025-11-27 20:02:36 +0300

    Добавлено перемещение сетки касанием

--- app/src/main/java/com/example/plotter/MainActivity.kt
+++ app/src/main/java/com/example/plotter/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.plotter.ui.theme.PlotterTheme
@@ -38,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(gridSize: Float = 30f, modifier: Modifier = Modifier) {
-    val lineColor = colorResource(id = R.color.black)
+    val lineColor = colorResource(id = R.color.gray)
 
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
@@ -46,28 +49,37 @@ fun Greeting(gridSize: Float = 30f, modifier: Modifier = Modifier) {
     var x = offsetX % gridSize
     var y = offsetY % gridSize
 
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        val width = size.width
-        val height = size.height
-
-        while (y <= height) {                   // ГОРИЗАНТАЛЬ
-            drawLine(
-                color = lineColor,
-                start = Offset(0f, y),
-                end = Offset(size.width, y),
-                strokeWidth = 4f
-            )
-            y += gridSize
+    Box(modifier = Modifier.pointerInput( Unit){
+        detectDragGestures{change, dragAmount ->
+            change.consume()
+            offsetX += dragAmount.x
+            offsetY += dragAmount.y
         }
+    }
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val width = size.width
+            val height = size.height
 
-        while (x <= width) {                    // ВЕРТИКАЛЬ
-            drawLine(
-                color = lineColor,
-                start = Offset(x, 0f),
-                end = Offset(x, size.height),
-                strokeWidth = 4f
-            )
-            x += gridSize
+            while (y <= height) {                   // ГОРИЗАНТАЛЬ
+                drawLine(
+                    color = lineColor,
+                    start = Offset(0f, y),
+                    end = Offset(size.width, y),
+                    strokeWidth = 4f
+                )
+                y += gridSize
+            }
+
+            while (x <= width) {                    // ВЕРТИКАЛЬ
+                drawLine(
+                    color = lineColor,
+                    start = Offset(x, 0f),
+                    end = Offset(x, size.height),
+                    strokeWidth = 4f
+                )
+                x += gridSize
+            }
         }
     }
 }
