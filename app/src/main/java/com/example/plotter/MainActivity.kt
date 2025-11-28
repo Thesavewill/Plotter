33fe06236b6f440daab18ba16ee0594fd13fa99d
Thesavewill <denes3040@gmail.com>
2025-11-28 23:18:01 +0300

    Линии сетки были разделены на мажорные и минорные; Добавлены коментарии; Упрощён код

--- app/src/main/java/com/example/plotter/MainActivity.kt
+++ app/src/main/java/com/example/plotter/MainActivity.kt
@@ -13,15 +13,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.plotter.ui.theme.PlotterTheme
+import kotlin.math.abs
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,64 +45,90 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(gridSize: Float = 70f, modifier: Modifier = Modifier) {
-    var lineColor1 = colorResource(id = R.color.gray)
-    var lineColor2 = colorResource(id = R.color.black)
+    val lineColor1 = colorResource(id = R.color.light_gray)   // ЗАРАНЕЕ ОПРЕДЕЛЯЕМ ЦВЕТА ДЛЯ Canvas
+    val lineColor2 = colorResource(id = R.color.gray)         // ЗАРАНЕЕ ОПРЕДЕЛЯЕМ ЦВЕТА ДЛЯ Canvas
+    val lineColor3 = colorResource(id = R.color.black)        // ЗАРАНЕЕ ОПРЕДЕЛЯЕМ ЦВЕТА ДЛЯ Canvas
 
-    var offsetX by remember { mutableStateOf(0f) }
-    var offsetY by remember { mutableStateOf(0f) }
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var offsetY by remember { mutableFloatStateOf(0f) }
 
-    Box(modifier = Modifier.pointerInput( Unit){
-        detectDragGestures{change, dragAmount ->
-            change.consume()
+    Box(modifier = Modifier.pointerInput( Unit){          //ДВИЖЕНИЕ СЕТКИ
+        detectDragGestures{change, dragAmount -> change.consume()
             offsetX += dragAmount.x
             offsetY += dragAmount.y
         }
     }
     ) {
         Canvas(modifier = Modifier.fillMaxSize()) {
-            val gridWidth = 5f
+            val gridWidth = 4f
 
             val width = size.width
             val height = size.height
 
-            var x = offsetX % gridSize
-            var y = offsetY % gridSize
+            var minorX = offsetX % gridSize
+            var minorY = offsetY % gridSize
 
-            while (y <= height) {                     //ГОРИЗОНТАЛЬ
+            var majorX = offsetX % gridSize
+            var majorY = offsetY % gridSize
+
+            //----------------Минорные линии сетки--------------
+            while (minorY < height) {                     //ГОРИЗОНТАЛЬНЫЕ МИНОРНЫЕ ЛИНИИ
                 drawLine(
                     color = lineColor1,
-                    start = Offset(0f, y),
-                    end = Offset(width, y),
+                    start = Offset(0f, minorY),
+                    end = Offset(width, minorY),
                     strokeWidth = gridWidth
                 )
-                y += gridSize
+                minorY += gridSize
             }
-
-            while (x <= width) {                      //ВЕРТИКАЛЬ
+            while (minorX < width) {                     //ВЕРТИКАЛЬНЫЕ МИНОРНЫЕ ЛИНИИ
                 drawLine(
                     color = lineColor1,
-                    start = Offset(x, 0f),
-                    end = Offset(x, height),
+                    start = Offset(minorX, 0f),
+                    end = Offset(minorX, height),
                     strokeWidth = gridWidth
                 )
-                x += gridSize
+                minorX += gridSize
             }
 
-            val offsetX = offsetX                       //Ось X
-            if (offsetX in 0f..width) {
+            //----------------Мажорные линии сетки--------------
+            while (majorY < height) {                       //ГОРИЗОНТАЛЬНЫЕ МАЖОРНЫЕ ЛИНИИ
+                if (((majorY - offsetY) / gridSize).roundToInt() % 4f == 0f) {
+                    drawLine(
+                        color = lineColor2,
+                        start = Offset(0f, majorY),
+                        end = Offset(width, majorY),
+                        strokeWidth = gridWidth
+                    )
+                }
+                majorY += gridSize
+            }
+            while (majorX < width) {                        //ВЕРТИКАЛЬНЫЕ МАЖОРНЫЕ ЛИНИИ
+                if (((majorX - offsetX) / gridSize).roundToInt() % 4f == 0f) {
+                    drawLine(
+                        color = lineColor2,
+                        start = Offset(majorX, 0f),
+                        end = Offset(majorX, height),
+                        strokeWidth = gridWidth
+                    )
+                }
+                majorX += gridSize
+            }
+
+            //----------------Ox и Oy--------------
+            if (offsetY < height) {                     //Ось X
                 drawLine(
-                    color = lineColor2,
-                    start = Offset(offsetX, 0f),
-                    end = Offset(offsetX, height),
+                    color = lineColor3,
+                    start = Offset(0f, offsetY),
+                    end = Offset(width, offsetY),
                     strokeWidth = gridWidth * 1.5f
                 )
             }
-            val offsetY = offsetY                       //Ось Y
-            if (offsetY in 0f..height) {
+            if (offsetX < width) {                      //Ось Y
                 drawLine(
-                    color = lineColor2,
-                    start = Offset(0f, offsetY),
-                    end = Offset(width, offsetY),
+                    color = lineColor3,
+                    start = Offset(offsetX, 0f),
+                    end = Offset(offsetX, height),
                     strokeWidth = gridWidth * 1.5f
                 )
             }
