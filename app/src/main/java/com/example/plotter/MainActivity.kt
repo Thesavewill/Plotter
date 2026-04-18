b76128e21ef4d00f91ebab1e0ba2a258f47f229a
Thesavewill <denes3040@gmail.com>
2026-04-19 00:43:44 +0300

    Добавление импортов

--- app/src/main/java/com/example/plotter/MainActivity.kt
+++ app/src/main/java/com/example/plotter/MainActivity.kt
@@ -53,6 +53,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
