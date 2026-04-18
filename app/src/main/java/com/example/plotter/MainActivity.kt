fc7600eb26d503047946e58c789a8398076733fd
Thesavewill <denes3040@gmail.com>
2026-04-19 00:42:50 +0300

    Добавление импортов

--- app/src/main/java/com/example/plotter/MainActivity.kt
+++ app/src/main/java/com/example/plotter/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
