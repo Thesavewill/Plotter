27bc6a45754db5b0367738cd69ec0d747c0643b1
Thesavewill <denes3040@gmail.com>
2026-05-09 17:39:21 +0300

    Добавлены импорты

--- app/src/main/java/com/example/plotter/MainActivity.kt
+++ app/src/main/java/com/example/plotter/MainActivity.kt
@@ -57,6 +57,8 @@ import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
