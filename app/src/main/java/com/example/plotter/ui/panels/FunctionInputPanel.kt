09a68bc8d13af1304f0a377bda11883a1e2e4113
Thesavewill <denes3040@gmail.com>
2026-05-15 19:38:56 +0300

    Добавлен импорт

--- app/src/main/java/com/example/plotter/ui/panels/FunctionInputPanel.kt
+++ app/src/main/java/com/example/plotter/ui/panels/FunctionInputPanel.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
